350662a31df30a5f4894f5d8647241d6ce5c229e
unknown <nipcts3@gmail.com>
2024-10-01 18:16:40 +0200

    added all assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img/>
+        </>
+      )}
+    </>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
